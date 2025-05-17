5f0f56538755136c8c50e1c304eddf90f5d2ece9
Игорь Надеин <gari.music126@yandex.ru>
2025-05-17 22:08:57 +0300

    models+users

--- backend/food/models.py
+++ backend/food/models.py
@@ -1,3 +1,89 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+User = get_user_model
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        max_length=32,
+        unique=True,
+        verbose_name='Уникальное название',
+    )
+    slug = models.SlugField(
+        max_length=32,
+        unique=True,
+        verbose_name='Уникальный слаг',
+    )
+
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        max_length=128,
+        verbose_name='Название',
+    )
+    measurement_unit = models.CharField(
+        max_length=64,
+        verbose_name='Единица измерения',
+    )
+
+
+class Recipe(models.Model):
+    """Модель рецептров."""
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name='Автор публикации (пользователь',
+    )
+    name = models.CharField(
+        max_length=256,
+        verbose_name='Название',
+    )
+    image = models.ImageField(
+        upload_to='recipes/',
+        verbose_name='Картинка',
+    )
+    text = models.TextField(
+        verbose_name='Текстовое описание',
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='IngredientRecipe',
+        verbose_name='Ингредиенты'
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Тег'
+    )
+    cooking_time = models.IntegerField(
+        validators=[
+            MinValueValidator(
+                1, 'Нужно указать время приготовления!'
+            )
+        ],
+        verbose_name='Время приготовления в минутах'
+    )
+
+
+class IngredientRecipe(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE
+    )
+    amount = models.IntegerField(
+        max_digits=6,
+        decimal_places=2,
+        default=1,
+        verbose_name='Количество ингредиентов',
+        validators=[
+            MinValueValidator(
+                1, 'Количество ингредиентов должно быть не менее 1!'
+            )
+        ]
+    )
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Модель пользователя."""
+    avatar = models.ImageField(
+        upload_to='avatars/',  # путь, куда будут загружаться файлы
+        blank=True,
+        null=True,
+        default='avatars/default.jpg'  # путь к дефолтной аватарке
+        verbose_name='Аватарка пользователя',
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('id',)
+
+    def __str__(self):
+        return self.username
+
+
+class Subscription(models.Model):
+    """Модель подписок."""
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',
+        verbose_name='Автор'
+    )
+    subscriber = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+        verbose_name='Подписчик'
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['subscriber', 'author'],
+                name='unique_subscriber_author'
+            )
+        ]
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+
+    def __str__(self):
+        return f'{self.subscriber} подписан на {self.author}'
