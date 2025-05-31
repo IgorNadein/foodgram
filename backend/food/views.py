5b87a61988b844237b105b469ac5cfc7fa430eb0
Игорь Надеин <gari.music126@yandex.ru>
2025-05-31 23:49:39 +0300

    401%

--- backend/food/views.py
+++ backend/food/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import redirect
+from django.http import HttpResponseRedirect
 
 
 def recipe_redirect(request, recipe_id):
     """Переход по короткой ссылке."""
-    return redirect(f'recipes/{recipe_id}/')
+    return HttpResponseRedirect(
+        request.build_absolute_uri(f'/recipes/{recipe_id}/')
+    )
